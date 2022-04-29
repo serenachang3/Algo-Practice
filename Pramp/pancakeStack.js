bcd1430959483115788ee202400c844aa67592c4
Serena Chang <serena.w.chang@gmail.com>
2022-04-29 14:47:15 -0400

    pancake sort with solution

--- Pramp/pancakeStack.js
+++ Pramp/pancakeStack.js
@@ -1,5 +1,21 @@
 function pancakeSort(arr) {
     // your code goes here
+    for (let i = arr.length-1 ; i >= 0 ; i--){
+      let maxIndex = findMaxIndexInPrefix(arr, i)
+      flip(arr, maxIndex)
+      flip(arr, i)
+    }
+    return arr
+  }
+  
+  function findMaxIndexInPrefix(arr, k){
+    let ans = 0;
+    for(let i = 0; i <= k ; i++){
+      if(arr[i] > arr[ans]){
+        ans = i
+      }
+    }
+    return ans
   }
   
   function flip(arr, k){
@@ -16,5 +32,5 @@ function pancakeSort(arr) {
     return arr;
   }
   
-  let answer = flip([1,2,3,4,5], 2)
+  let answer = pancakeSort([5,4,3,2,1])
   console.log(answer)
