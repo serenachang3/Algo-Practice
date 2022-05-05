509082e02f71784cccb8c7a2e6337f29d6d6fc0e
Serena Chang <serena.w.chang@gmail.com>
2022-05-05 17:36:14 -0400

    sliding window solution

--- LeetCode/String/longest-substring-without-repeating-charaters.js
+++ LeetCode/String/longest-substring-without-repeating-charaters.js
@@ -80,3 +80,19 @@ var lengthOfLongestSubstring = function (s) {
 // The time complexity of this solution is linear, O(n), since we solved the problem by having to pass through the string only once.
 
 
+var lengthOfLongestSubstring = function(s) {
+    var maxLength = 0
+    var left = 0
+    var charSet = new Set()
+    
+    for (var right = 0; right < s.length; right++){
+        while (charSet.has(s[right])){
+            charSet.delete(s[left])
+            left += 1
+        }
+        charSet.add(s[right])
+        maxLength = Math.max(maxLength, right - left + 1)
+    }
+    
+    return maxLength
+};
