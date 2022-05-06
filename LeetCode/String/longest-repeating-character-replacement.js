1d6bcdd57df949084ab92c9a1fb85b487dd86cea
Serena Chang <serena.w.chang@gmail.com>
2022-05-05 22:37:01 -0400

    solution with my own variable names

--- LeetCode/String/longest-repeating-character-replacement.js
+++ LeetCode/String/longest-repeating-character-replacement.js
@@ -58,18 +58,23 @@ var characterReplacement = function(s, k) {
 
 var characterReplacement = function(s, k) {
     // Keep count of all the characters in the string
-    const chars = {}
+    const hash = {}
     // left pointer, character with the current max frequency, return value
-    let left = 0, maxf = 0, output = 0
+    let left = 0;
+    let maxFrequencyOfLetter = 0;
+    let output = 0;
     for(let right = 0; right < s.length; right++) {
-        const char = s[right]
-        // Increment count of the current character
-        chars[char] = 1 + (chars[char] || 0)
+        const currLetter = s[right]
+        // Increment count of the current character and OR add the key to the hashtable/chars
+        hash[currLetter] = 1 + (hash[currLetter] || 0)
         // Update the character frequency
-        maxf = Math.max(maxf, chars[char])
-        // Shrink the window of characters we are looking at until we can have a window of all the same characters + k charcters to change
-        while((right-left+1) - maxf > k) {
-                chars[s[left]] -= 1
+        maxFrequencyOfLetter = Math.max(maxFrequencyOfLetter, hash[currLetter])
+        // Shrink the window of characters we are looking at (from left) until we can have a window of all the same characters + k charcters to change
+        //(right-left+1) << you add the +1 becuase we are starting from index
+        while((right-left+1) - maxFrequencyOfLetter > k) { 
+                //you subtract from the left most character key-value because you shift the frame over & no longer include it
+                hash[s[left]] -= 1 
+                //increase the left value since  you are moving frame
                 left++
           }
         // Update the output if the current window is greater than our previous max window
