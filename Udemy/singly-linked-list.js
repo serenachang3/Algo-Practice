9b8c8cdfcff9a4922a3b8e4cd4af1cb862164cab
Serena Chang <serena.w.chang@gmail.com>
2022-05-08 18:17:13 -0400

    push, pop, shift

--- Udemy/singly-linked-list.js
+++ Udemy/singly-linked-list.js
@@ -13,6 +13,61 @@ class SinglyLinkedList {
     }
 
     push(val){
-        
-    }
+        if(this.head === null){
+            this.head = new Node(val);
+            this.tail = this.head;
+        } else {
+            this.tail.next = new Node(val);
+            this.tail = this.tail.next;
+        }
+        this.length++;
+        return this;
+    };
+  
+    pop(){
+      
+      if(!this.head) undefined;
+      
+      let curr = this.head;
+      let secondToLast = curr;
+      
+      while(curr.next){
+        secondToLast = curr
+        curr = curr.next;
+      }
+       
+      this.tail = secondToLast;
+      this.tail.next = null;
+      this.length--;
+      if(this.length === 0) {
+        this.head = null;
+        this.tail = null;
+      }
+      return curr;
+    };
+  
+    shift(){
+      
+      if(!this.head) return undefined;
+      
+      let currHead = this.head;
+      let newHead = currHead.next;
+      
+      this.head = newHead;
+      this.length--;
+      if(this.length === 0) {
+        this.tail = null;
+      }
+      return currHead;
+    };
 }
+
+let list = new SinglyLinkedList()
+list.push("How");
+list.push("are")
+list.push("you")
+list.push('?')
+
+console.log("shift", list.shift())
+console.log("new head", list.head)
+
