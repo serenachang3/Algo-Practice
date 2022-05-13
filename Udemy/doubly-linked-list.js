3c304f7877f2aecee653a07dc1f71abe8aab15af
Serena Chang <serena.w.chang@gmail.com>
2022-05-12 20:52:27 -0400

    push method

--- Udemy/doubly-linked-list.js
+++ Udemy/doubly-linked-list.js
@@ -12,4 +12,19 @@ class Node{
       this.tail = null;
       this.length = 0;
     }
+
+    push(val){
+        let newNode = new Node(val);
+        
+        if(this.head === null){
+          this.head = newNode;
+          this.tail = newNode;
+        } else {
+          this.tail.next = newNode;
+          newNode.prev = this.tail; //similar to SLL but need to link it both ways
+          this.tail = newNode;
+        }
+        this.length++;
+        return this;
+      };
   }
