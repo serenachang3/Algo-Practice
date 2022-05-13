85d41b8143f5f33e5a0269969557c56c3d313114
Serena Chang <serena.w.chang@gmail.com>
2022-05-12 21:35:03 -0400

    pop

--- Udemy/doubly-linked-list.js
+++ Udemy/doubly-linked-list.js
@@ -27,4 +27,22 @@ class Node{
         this.length++;
         return this;
       };
+
+      pop(){    
+        if(!this.head) return undefined;
+        
+        let oldTail = this.tail;
+        
+        if(this.length === 1){
+          this.head = null;
+          this.tail = null;
+        } else {
+          this.tail = oldTail.prev;
+          this.tail.next = null;
+          oldTail.prev = null;
+        }
+        this.length--;
+        return oldTail;  
+      };
+      
   }
