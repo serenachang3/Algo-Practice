4405c4a210ed987bab28f63d4e9f54b61db59dd3
Serena Chang <serena.w.chang@gmail.com>
2022-05-13 13:27:30 -0400

    get

--- Udemy/doubly-linked-list.js
+++ Udemy/doubly-linked-list.js
@@ -1,81 +1,98 @@
-class Node{
-    constructor(val){
-      this.val = val;
-      this.next = null;
-      this.prev = null;
-    }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
   }
-  
-  class DoublyLinkedList {
-    constructor(){
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail; //similar to SLL but need to link it both ways
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let oldTail = this.tail;
+
+    if (this.length === 1) {
       this.head = null;
       this.tail = null;
-      this.length = 0;
+    } else {
+      this.tail = oldTail.prev;
+      this.tail.next = null;
+      oldTail.prev = null;
     }
-
-    push(val){
-        let newNode = new Node(val);
-        
-        if(this.head === null){
-          this.head = newNode;
-          this.tail = newNode;
-        } else {
-          this.tail.next = newNode;
-          newNode.prev = this.tail; //similar to SLL but need to link it both ways
-          this.tail = newNode;
-        }
-        this.length++;
-        return this;
-      };
-
-      pop(){    
-        if(!this.head) return undefined;
-        
-        let oldTail = this.tail;
-        
-        if(this.length === 1){
-          this.head = null;
-          this.tail = null;
-        } else {
-          this.tail = oldTail.prev;
-          this.tail.next = null;
-          oldTail.prev = null;
-        }
-        this.length--;
-        return oldTail;  
-      };
-
-      shift(){
-        if(!this.head) return undefined;
-        
-        let headNode = this.head
-        
-        if(this.length === 1){
-          this.head = null;
-          this.tail = null;
-        } else {
-          this.head = headNode.next;
-          this.head.prev = null;
-          headNode.next = null;
-        }
-        this.length--;
-        return headNode; 
-      };
-
-      unshift(val){
-        let newNode = new Node(val);
-
-        if(this.length === 0){
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.head.prev = newNode;
-            newNode.next = this.head;
-            this.head = newNode;
-        }
-        this.length++;
-        return this;
-      };
-      
-
+    this.length--;
+    return oldTail;
   }
+
+  shift() {
+    if (!this.head) return undefined;
+
+    let headNode = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = headNode.next;
+      this.head.prev = null;
+      headNode.next = null;
+    }
+    this.length--;
+    return headNode;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(idx) {
+    if (idx < 0 || idx >= this.length) return null;
+    if (idx <= this.length / 2) {
+      console.log("working from start");
+      let node = this.head;
+      for (let i = 0; i < idx; i++) {
+        node = node.next;
+      }
+      return node;
+    } else {
+      console.log("working from end");
+      let node = this.tail;
+      for (let i = this.length - 1; i > idx; i--) {
+        node = node.prev;
+      }
+      return node;
+    }
+  }
+}
