bc8e694573f3d94db9df5d7945ef2ec4abc6f258
Serena Chang <serena.w.chang@gmail.com>
2022-05-19 17:34:47 -0400

    call stack

--- Udemy/recursion.js
+++ Udemy/recursion.js
@@ -2,3 +2,16 @@ function sumRange(num){
     if(num ===1) return 1;
     return num + sumRange(num-1)
 }
+
+sumRange(3)
+
+// callstack
+
+// return 3 + sumRange(2)
+    // return  2 + sumRange(1)
+        //  return 1
+
+        // return 2  + 1
+    // return 3 + 3
+// FINAL RETURN 6
+
