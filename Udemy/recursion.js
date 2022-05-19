9ce2c00151b005b4fbaa9f6de75fd684954f29de
Serena Chang <serena.w.chang@gmail.com>
2022-05-19 18:10:10 -0400

    helper function

--- Udemy/recursion.js
+++ Udemy/recursion.js
@@ -18,6 +18,20 @@ sumRange(3)
 
 function factorial(num){
     if(num === 1) return 1;
-
     return num * factorial(num-1)
+}
+
+
+// Helper Method Recursion
+
+function outer(input){
+    let outerScopedVariable = [];
+
+    function helper(helperInput){
+        //modify the outerScopedVariable
+        helper(helperInput--)
+    }
+
+    helper(input);
+    return outerScopedVariable;
 }
