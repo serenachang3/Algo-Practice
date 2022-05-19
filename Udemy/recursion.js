3531793a8877244960c71f2a94a432882e355774
Serena Chang <serena.w.chang@gmail.com>
2022-05-19 18:18:02 -0400

    another example of helper

--- Udemy/recursion.js
+++ Udemy/recursion.js
@@ -35,3 +35,19 @@ function outer(input){
     helper(input);
     return outerScopedVariable;
 }
+
+function collectOddValues(arr){
+    let result = [];
+
+    function helper(helperInput){
+        if(helperInput.length  === 0){
+            return;
+        }
+        if(helperInput[0] % 2 !== 0){
+            result.push(helperInput[0])
+        }
+        helper(helperInput.slice(1)) // recursion: take off the first number since you already checked
+    }
+    helper(arr);
+    return result;
+}
