07cab9e4860407beb7bec8a43e50863db7eba766
Serena Chang <serena.w.chang@gmail.com>
2022-05-19 21:27:10 -0400

    recursive range

--- Udemy/recursion.js
+++ Udemy/recursion.js
@@ -122,3 +122,15 @@ function productOfArray(arr){
   return arr[0] * productOfArray(arr.slice(1))
   
 }
+
+// Recursive  Range
+
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55 
+
+function recursiveRange(num){
+  if(num === 1) return 1;
+  
+  return num + recursiveRange(num-1)
+}
