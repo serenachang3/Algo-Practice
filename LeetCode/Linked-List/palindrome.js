231cc3cb6edac61933e257068ffac1647c2d85dc
Serena Chang <serena.w.chang@gmail.com>
2022-05-20 18:15:52 -0400

    second approach

--- LeetCode/Linked-List/palindrome.js
+++ LeetCode/Linked-List/palindrome.js
@@ -38,3 +38,30 @@ var isPalindrome = function(head) {
     return true;
     
 };
+
+// Approach 2 Space O(1) Time O(n)
+var isPalindrome = function (head) {
+    let [fast, slow] = [head, head];
+    // To find middle
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    // Reverse the 2nd half of the linked list to get a new reversed list
+    let [curr, prev] = [slow, null];
+    while (curr) {
+        const next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    let [left, right] = [head, prev];
+    while (right) {
+        if (left.val !== right.val) return false;
+        left = left.next;
+        right = right.next;
+    }
+
+    return true;
+};
